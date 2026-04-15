fa51bc694124e65453e1547c8758ba9e2035671e
ampats04 <jeremyandyampatin@gmail.com>
2026-04-16 02:17:21 +0800

    fix(test): ensured config is disabled during test in workflowS

--- src/test/java/com/accenture/bankingservices/BankingServicesApplicationTests.java
+++ src/test/java/com/accenture/bankingservices/BankingServicesApplicationTests.java
@@ -3,7 +3,9 @@ package com.accenture.bankingservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.cloud.config.enabled=false"
+})
 class BankingServicesApplicationTests {
 
 	@Test
